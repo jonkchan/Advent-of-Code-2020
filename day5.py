7d322ee3856c1ee3073dd5943a83431c0dcf9771
Jonathan Chan <johnkchan252@gmail.com>
2020-12-05 14:11:52 -0500

    Update day5.py

--- day5.py
+++ day5.py
@@ -18,13 +18,16 @@ class Day5:
     def part1(self):
         highest_seat_id = 0
         for boarding_pass in self.key:
+            # First 7 characters will either be F or B to determine the seat row
+            # Last 3 characters will either be L or R to determine the seat column
             row_directions, col_directions = boarding_pass[:6], boarding_pass[-3:]
+
             row = self._space_finder(row_directions, 128)
             col = self._space_finder(col_directions, 8)
+
+            # Unique seat ID found by multiplying seat row by 8 then adding seat column
             seat_id = (row * 8) + col
 
-            if col == 2 and row > 100:
-                print(boarding_pass, row, col, seat_id)
             if seat_id > highest_seat_id:
                 highest_seat_id = seat_id
 
