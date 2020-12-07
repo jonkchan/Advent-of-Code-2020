e57f913834a8e2f2ca44cce23ed58d1fd0633110
Jonathan Chan <johnkchan252@gmail.com>
2020-12-06 20:57:00 -0500

    Update day5.py

--- day5.py
+++ day5.py
@@ -15,29 +15,41 @@ class Day5:
                 spaces = spaces[midpoint:]
         return min(spaces)
 
-    def part1(self):
+    @staticmethod
+    def _calculate_seat_id(boarding_pass: str) -> int:
+        # First 7 characters will either be F or B to determine the seat row
+        # Last 3 characters will either be L or R to determine the seat column
+        row_directions, col_directions = boarding_pass[:7], boarding_pass[-3:]
+
+        row = Day5._space_finder(row_directions, 128)
+        col = Day5._space_finder(col_directions, 8)
+
+        # Unique seat ID found by multiplying seat row by 8 then adding seat column
+        seat_id = (row * 8) + col
+        return seat_id
+
+    def part1(self) -> int:
         highest_seat_id = 0
         for boarding_pass in self.key:
-            # First 7 characters will either be F or B to determine the seat row
-            # Last 3 characters will either be L or R to determine the seat column
-            row_directions, col_directions = boarding_pass[:6], boarding_pass[-3:]
-
-            row = self._space_finder(row_directions, 128)
-            col = self._space_finder(col_directions, 8)
-
-            # Unique seat ID found by multiplying seat row by 8 then adding seat column
-            seat_id = (row * 8) + col
+            seat_id = self._calculate_seat_id(boarding_pass)
 
             if seat_id > highest_seat_id:
                 highest_seat_id = seat_id
 
         return highest_seat_id
 
-    def part2(self):
-        return
+    def part2(self) -> int:
+        seat_ids = [self._calculate_seat_id(
+            boarding_pass) for boarding_pass in self.key]
+
+        seat_ids = sorted(seat_ids)
+
+        for idx, seat_id in enumerate(seat_ids):
+            if seat_id != seat_ids[idx - 1] + 1:
+                return seat_ids[idx - 1] + 1
 
 
 if __name__ == "__main__":
     day5 = Day5("day5.txt")
     print(day5.part1())
-    # print(day5.part2())
+    print(day5.part2())
